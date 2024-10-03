a11ee4d8568db2e5363e0d66e84a529407c5e66b
oscar2697 <77596284+oscar2697@users.noreply.github.com>
2024-10-03 17:20:27 -0500

    "Updated templates, views, and URLs for login, logout, and registration functionality"

--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    if request.method == 'post':
-        username = request.post['username']
-        password = request.post['password']
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
         user = authenticate(request, username=username, password=password)
 
@@ -21,4 +22,27 @@ def home(request):
         return render(request, 'home.html', {})
 
 def logout_user(request):
-    
+    logout(request)
+    messages.success(request, 'You Have Been Logged Out...')
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form =  SignUpForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            
+            login(request, user)
+            messages.success(request, 'You Have Succesfully Registered')
+            return redirect('home')
+        
+    else: 
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
